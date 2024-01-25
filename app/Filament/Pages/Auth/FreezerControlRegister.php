6c9603e4fb6714c043752c5b13c31492c0557f59
Anderson da Mata <anderson.daamata@gmail.com>
2024-01-25 13:14:11 -0300

    chore: small improvements on age checking function

--- app/Filament/Pages/Auth/FreezerControlRegister.php
+++ app/Filament/Pages/Auth/FreezerControlRegister.php
@@ -2,13 +2,19 @@
 
 namespace App\Filament\Pages\Auth;
 
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use Filament\Events\Auth\Registered;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Http\Responses\Auth\Contracts\RegistrationResponse;
+use Filament\Notifications\Notification;
 use Filament\Pages\Auth\Register;
 
 class FreezerControlRegister extends Register
 {
+    public ?array $data = [];
     public function form(Form $form): Form
     {
         return $form
@@ -45,6 +51,58 @@ class FreezerControlRegister extends Register
                     ->label('Data de Nascimento')
                     ->required()
             ]);
-
     }
+
+    #[\Override]
+    public function register(): ?RegistrationResponse
+    {
+        try {
+            $this->rateLimit(2);
+
+            $this->data = $this->form->getState();
+
+            if (!$this->checkIfCustomerHasMore18YearsOld()) {
+                throw new \DomainException('Infelizmente, você não tem idade para se cadastrar no '. config('app.name'));
+            }
+
+        } catch (TooManyRequestsException $exception) {
+            Notification::make()
+                ->title(__('filament-panels::pages/auth/register.notifications.throttled.title', [
+                    'seconds' => $exception->secondsUntilAvailable,
+                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+                ]))
+                ->body(array_key_exists('body', __('filament-panels::pages/auth/register.notifications.throttled') ?: []) ? __('filament-panels::pages/auth/register.notifications.throttled.body', [
+                    'seconds' => $exception->secondsUntilAvailable,
+                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+                ]) : null)
+                ->danger()
+                ->send();
+
+            return null;
+        } catch (\DomainException $domainException) {
+            Notification::make()
+                ->title('Erro ao realizar cadastro')
+                ->body($domainException->getMessage())
+                ->danger()
+                ->send();
+        }
+
+        $user = $this->getUserModel()::create($this->data);
+
+        event(new Registered($user));
+
+        $this->sendEmailVerificationNotification($user);
+
+        Filament::auth()->login($user);
+
+        session()->regenerate();
+
+        return app(RegistrationResponse::class);
+    }
+
+    private function checkIfCustomerHasMore18YearsOld(): bool
+    {
+        return now()->parse($this->data['birthdate'])->age >= 18;
+    }
+
 }
